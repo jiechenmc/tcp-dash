98cbe52755f2180bd5afa00bdc49fbc84e7cc074
Jie Chen <jiechenmc@gmail.com>
2025-03-12 22:43:12 -0400

    ebpf removed

--- ebpf/go.mod
+++
@@ -1,7 +0,0 @@
-module ebpf-test
-
-go 1.24.1
-
-require github.com/cilium/ebpf v0.17.3
-
-require golang.org/x/sys v0.30.0 // indirect
--- ebpf/setup.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get update && \
-    sudo apt-get install -y build-essential git cmake \
-    zlib1g-dev libevent-dev \
-    libelf-dev llvm libbpf-dev \
-    clang libc6-dev \
-    wget gcc-multilib linux-headers-generic
-
-wget https://go.dev/dl/go1.24.1.linux-amd64.tar.gz && sudo tar -C /usr/local -xzf go1.24.1.linux-amd64.tar.gz
-rm go1.24.1.linux-amd64.tar.gz
