6e7b7cd33879c6b8d79ba6442d626d7494698c25
Jie Chen <jiechenmc@gmail.com>
2025-04-27 13:42:21 -0400

    latest update

--- sync_branches.sh
+++ sync_branches.sh
@@ -7,6 +7,8 @@ cd dash.js
 for branch in "${sub_branches[@]}"; do
     echo $branch
     git switch $branch
+    git config pull.rebase false
+    git pull
     git merge development
     git push
 done
